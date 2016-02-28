7ebfbf498653396be35159a4df9c14119678fae7
thomasgzh <thomasgzh@gmail.com>
2016-02-28 19:44:05 +0800

    Added files via upload

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
+MEAN Typescript Angular2 Skeleton
+=================
+
+A simple MEAN stack skeleton written in Typescript.
+
+### Features
+* MongoDB
+* Express
+* Angular 2
+* Node.js
+* Websockets
+* Mongoose
+
+### Dependencies
+
+* Node
+* MongoDB
+* Typescript
+
+## Running
+
+Note: All commands entered need to be performed from within *this directory*.
+
+1. Ensure you have a clean directory to dedicate as to a database (e.g. `C:\database` or `~/database/`).
+
+2. From *this repository's directory*, run the following command to launch the MongoDB process.
+    ```shell
+    <MONGO_INSTALL_DIRECTORY>/bin/mongod --dbpath <PATH_TO_DB_DIRECTORY>
+    ```
+
+3. From this directory, install the app's node dependencies, tsd, and typings with the following commands:
+    ```shell
+    npm install
+    npm install -g tsd
+    tsd install
+    ```
+
+4. Before compiling the app you may have to change permissions (default is 744 after tsd install) to the typings directory.
+    ```shell
+    chmod -R 774 typings
+    ```
+
+5. Compile the app with the following command:
+    ```shell
+    tsc
+    cd public
+    tsc
+    ```
+
+7. Launch the Node process to serve the app using the following command from the root folder:
+    ```shell
+    node server.js
+    ```
+
+7. Open your favorite browser and navigating to `http://localhost:3000/` to access the app.
+
+
 # MEAN-ts-ng2-appframework
-This is a sample app framework for angularjs 2.
---
+++ server.ts
@@ -0,0 +1,75 @@
+/// <reference path="./typings/tsd.d.ts"/>​
+
+var express = require('express')
+var bodyParser = require('body-parser')
+
+import http = require('http');
+import path = require('path');
+import mongoose = require('mongoose');
+import WebSocket = require('ws');
+
+import routes = require('./server/resources/index');
+import documentRoutes = require('./server/resources/document');
+import todoRoutes = require('./server/resources/todos');
+import models = require('./server/dao/messageModel');
+
+var wsPort: number = process.env.PORT || 3001;
+var databaseUrl: string = 'localhost';
+var httpPort = 3000;
+
+var WebSocketServer = WebSocket.Server;
+var server = new WebSocketServer({ port: wsPort });
+
+server.on('connection', ws => {
+	ws.on('message', message => {
+		try {
+			var userMessage: models.UserMessage = new models.UserMessage(message);
+			broadcast(JSON.stringify(userMessage));
+			if (userMessage.name == "document") {
+					documentRoutes.updateDocumentText(userMessage.message);
+			}
+		} catch (e) {
+			console.error(e.message);
+		}
+	});
+});
+
+function broadcast(data: string): void {
+	server.clients.forEach(client => {
+		client.send(data);
+	});
+};
+
+if (mongoose.connect('mongodb://'+databaseUrl+'/dbTexd')){
+	console.log("Successfully connected to the database: "+databaseUrl);
+} else {
+	console.log("Not able to connect to the database: "+databaseUrl);
+}
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.get('/', routes.index);
+app.get('/document', documentRoutes.read);
+app.get('/documentapp', routes.index);
+app.get('/todoapp', routes.index);
+app.post('/document', documentRoutes.update);
+app.get('/todos',todoRoutes.readAll);
+app.get('/todo/:id',todoRoutes.readOne);
+app.post('/todo',todoRoutes.save);
+app.put('/todo/:id',todoRoutes.update);
+app.delete('/todo/:id',todoRoutes.del);
+
+
+
+
+app.listen(httpPort, function(){
+    console.log("Demo Express server listening on port %d", httpPort);
+});
+
+export var App = app;
